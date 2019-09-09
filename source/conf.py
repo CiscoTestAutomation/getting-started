0c59f46c982c5f436b3ed7533eaea1397828ea89
kacann <kacann@cisco.com>
2019-09-09 11:05:29 -0400

    Added pdf option.

--- source/conf.py
+++ source/conf.py
@@ -37,6 +37,11 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# -- Options for PDF output -------------------------------------------------
+#
+pdf_documents = [('index')]
+
+
 
 # -- Options for HTML output -------------------------------------------------
 
